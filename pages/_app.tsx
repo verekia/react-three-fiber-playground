86966706edf4318bcb770a8510074537ec1d3773
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2021-09-02 13:23:52 +0700

    Basic cubes

---
+++ pages/_app.tsx
@@ -0,0 +1,5 @@
+import '../styles.css'
+
+const App = ({ Component, pageProps }) => <Component {...pageProps} />
+
+export default App
---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from 'react'
+
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Box(props) {
+  // This reference will give us direct access to the THREE.Mesh object
+  const ref = useRef(null)
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+const IndexPage = () => (
+  <Canvas>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas>
+)
+
+export default IndexPage
